997b1313e18cdaedf9d33c8f616f50920179d1cb
Phanidhar Beeram <phanidharbeeram@gmail.com>
2020-05-01 15:29:45 +0530

    newtrail2

---
+++ build/precache-manifest.5a1d9c7c9d9d4269917aa5a14893f500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8084a5a19a80eef2d99b84a7d343846e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aeddd6252432eaa288e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "984a5f6d0c3ee81f0bf1",
+    "url": "/static/js/2.acdf1c22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acdf1c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aeddd6252432eaa288e",
+    "url": "/static/js/main.cba371d8.chunk.js"
+  },
+  {
+    "revision": "ec61c65368545c561df0",
+    "url": "/static/js/runtime-main.874acf0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express();
 const path =require('path')
 const port=process.env.PORT || 4000;
-app.use(express.static('../check11/build/'))
+app.use(express.static('./build'))
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,"build","index.html"))
 })
